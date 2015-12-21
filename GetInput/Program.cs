8d5187e2955919f8540a380f1bbee9eb7c3d5415
gus07ven <gus07ven@gmail.com>
2015-12-21 15:27:39 -0600

    Add solution to problem 2.

--- GetInput/Program.cs
+++ GetInput/Program.cs
@@ -15,8 +15,51 @@ namespace PracticeApp
 
             Console.WriteLine("Hello, {0} the current time is {1}", userName, System.DateTime.Now.TimeOfDay);
 
+            Console.WriteLine("{0}, the solution to problem 1 is {1} ", userName, getSum());
+            Console.WriteLine("{0}, the solution to problem 2 is {1} ", userName, getFiboSum());
             Console.WriteLine("Press any key to quit.");
             Console.ReadKey();
         }
+
+        static int getSum()
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        static int getFiboSum()
+        {
+                int a, b, c;
+                int numLim = 0;
+                a = 1;
+                b = 2;
+                int sum = 0;
+
+                while (numLim < 4000000)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    numLim = b;
+                    if (numLim > 4000000)
+                    {
+                        break;
+                    }
+                    if (numLim % 2 == 0)
+                    {
+                        sum = sum + numLim;
+                    }
+                }
+                return sum;
+            }
+
+        }
     }
-}
+
